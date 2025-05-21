7dce23d9ab7aff76718f10390ba8672201d42e61
lincolnet01 <kamanoabra2014@outlook.com>
2025-05-21 18:13:22 +0000

    Invinting code rabbit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the underworld! 
+          Welcome to the underworld, my CodeRabbit! 
         </p>
         <a
           className="App-link"
